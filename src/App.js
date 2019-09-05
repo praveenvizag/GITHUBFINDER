21730deac32f7a1176f1f4022ef951a7958aa37e
Praveen Kumar Konchada <praveen.konchada@e2open.com>
2019-09-05 07:52:17 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layouts/navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
+import { setTimeout } from "timers";
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert : null
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data, loading: false });
+  }
+
+  //Search Git Hub Users
+  searchUsers = async text => {
+    console.log(`https://api.github.com/users?q=${text}`);
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users:res.data.items,loading:false});
+  };
+  clearUsers = () =>{
+  this.setState({users:[],loading:false})
+  }
+  //set Alerts
+  setAlert = (msg,type) => {
+    this.setState({alert:{msg,type}});
+      setTimeout(()=>
+        this.setState({alert:null}),5000
+
+         )
+  
+
+  }
+  
+  render() {
+    const {users,loading,alert} = this.state;
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Alert alert = {alert}/>
+          <Search searchUsers={this.searchUsers} clearUsers = {this.clearUsers} 
+          showClear = {this.state.users.length>0 ? true:false}  setAlert = {this.setAlert}/>
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
